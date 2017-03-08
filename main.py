9b96682c30631fec74c4f9fc0294c24ee2735052
DylanAlcock <dylan.alcock@gmail.com>
2017-03-07 22:18:10 -0700

    Moved home and functions to functions.py
    Moved home_page and functions defs to functions.py.
    Added functionality to login that allowed the user to go back to login/register screen.
    Closed cursor and connection at end.

--- main.py
+++ main.py
@@ -39,64 +39,25 @@ while state != EXIT:
     if state == LOGIN:
         user_login = login(cur)
         # infinite tries could make a limited amout
-        while user_login(0) != 1:
+        while user_login(0) != 1 and user_login[0] != 2:
             user_login = login(cur)
         if user_login(0) == 1:
             while user_login(0) = LOGGED_IN
                 home_page()
-        elif
+                functions()
+                state = EXIT
+                break
+             
+        elif user_login[0] == 2:
+            state = WELCOME
+        
+        else
             print("Login function error")
 
     elif state == REGISTER:
         if register(cur) == 1:
             con.commit()
             state = LOGIN
-
             
-def home_page()
-'''
-Provides the opening homepage for initial user login. It explains
-some controls, and displays tweets of followed users 5 at a time.
-There is an option to go and use the program functions for more
-advanced usage.
-'''
-    print("Welcome to Twitterpated! Here are all your followed users' tweets:")
-    # @TODO need to get both tweets and retweets
-    cur.execute("select tid, writer, usr, tdate, text, replyto " +
-                "from tweets t, users u " +
-                "where t1.writer = u1.usr " + 
-                "union " + 
-                "select tid, writer, usr, rdate, text " + 
-                "from retweets r join users u1 on r. users u2, )
-
-
-# Provides a menu for the functions of the program
-def functions():
-    print("Welcome to Twitterpated! The functions of Twitterpated are listed below.")
-
-    print("1 - Search for Tweets\n2 - Search for Users\n3 - Write a "
-          "Tweet\n 4 - List Followers\n5 - Manage Lists\n6 - Logout")
-    f_input = input("What would you like to do? ")
-
-    while f_input:
-        if f_input == "1":
-            search_tweet(cur)
-        elif f_input == "2":
-            search_user(cur)
-        elif f_input == "3":
-            write_tweet(cur, user_login)
-        elif f_input == "4":
-            list_followers(cur)
-        elif f_input == "5":
-            manage_lists(cur)
-        elif f_input == "6":
-            print("Logging out of Twitterpated.")
-            return 1
-        else:
-            print("The input entered was not valid. Please enter one of specified prompts.")
-
-        print("1 - Search for Tweets\n2 - Search for Users\n3 - Write a "
-              "Tweet\n 4 - List Followers\n5 - Manage Lists\n6 - Logout")
-        f_input = input("What would you like to do? ")
-
-    return 1
+cur.close()
+con.close()         
