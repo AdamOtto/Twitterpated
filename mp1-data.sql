d6781a121976695baf41866423dd9cb2b4e30b61
DylanAlcock <dylan.alcock@gmail.com>
2017-03-11 11:32:11 -0700

    added more test data

--- mp1-data.sql
+++ mp1-data.sql
@@ -23,13 +23,39 @@ insert into tweets values (4,1,'06-JUN-2016','My apologies #edmonton, the Metro 
 insert into tweets values (5,6,'25-JUL-2016','First tweet! Hello world! Hello #Edmonton!',null);
 insert into tweets values (6,5,'25-JUL-2016','bout to drop the hottest mixtape of 2064!!!!',5);
 insert into tweets values (7,7,'02-JUN-1989','IM A VAMPIRE! IM A VAMPIRE! IM A VAMPIRE!',null);
+--new
+insert into tweets values (8,7,'17-DEC-2016','Dont talk to me or my son or my grandson ever again #trump',null);
+insert into tweets values (9,7,'16-DEC-2016','#America, this is not what we meant when we said orange is the new black..',null);
+insert into tweets values (10,7,'29-JAN-2016','The music video for Bad Blood is out now!' ,null);
+insert into tweets values (11,7,'18-NOV-2014','We''re breaking up? Time to make another million bucks!',null);
+insert into tweets values (12,1,'14-FEB-2016','Roses are red chocolate is brown expected nothing and still feel down #rosesRred',null);
+insert into tweets values (13,1,'15-FEB-2016','Roses are red I love my wife she was the biggest mistake of my life #rosesRred',null);
+insert into tweets values (14,1,'09-JUL-2016','WE MUST MAKE AMERICA GREAT AGAIN!',null);
+insert into tweets values (15,1,'13-JUL-2016','Roses are red rice is plain please vote me into office again america #rosesRred',null);
+insert into tweets values (16,7,'15-DEC-2016','Can''t stump the #trump! #america',null);
 
 --hashtags(term)
 insert into hashtags values ('edmonton');
+--new
+insert into hashtags values ('trump');
+insert into hashtags values ('rosesRed');
+insert into hashtags values ('america');
+
 
 --mentions(tid,term)
 insert into mentions values (4, 'edmonton');
 insert into mentions values (5, 'edmonton');
+--new
+insert into mentions values (16, 'trump');
+insert into mentions values (8, 'trump');
+insert into mentions values (9, 'america');
+insert into mentions values (16, 'america');
+insert into mentions values (15, 'rosesRred');
+insert into mentions values (12, 'rosesRred');
+insert into mentions values (13, 'rosesRred');
+
+
+
 
 --retweets(usr,tid,rdate)
 insert into retweets values (2,1,'12-JAN-2017');
